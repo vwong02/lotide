119d477f53f10ff96d04bb98d44818dc391b80a7
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-14 02:09:45 -0500

    updated assertEqual function  outputs to be template strings

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,13 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: [actual] === [expected]");
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑🛑🛑 Assertion Failed: [actual] !== [expected]");
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
 
 assertEqual(3, 3);
+assertEqual("Lighthouse", "Bootcamp");
+assertEqual(3, 56);
+assertEqual("cheese", "CHEESE");
