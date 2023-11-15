0701ea948fd4832a352bf8daaeb2d3d8b343a185
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-15 01:44:20 -0500

    updated eqArray function

--- eqArrays.js
+++ eqArrays.js
@@ -8,22 +8,18 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(x, y) {
-  if (x.length !== y.length) {
-    return false
-  }
   for (let i = 0; i < x.length; i++) {
-    if (x[i] !== y[i]) {
-      return false
-    } else {
+    if (x[i] === y[i]) {
       return true
+    } else {
+      return false
     }
   }
   }
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
