a504bb5f81b6d55bd1871aa62420f7292154841e
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-15 01:36:22 -0500

    added file

---
+++ eqArray.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(x, y) {
+  if (x.length !== y.length) {
+    return false
+  }
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] !== y[i]) {
+      return false
+    } else {
+      return true
+    }
+  }
+  }
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
