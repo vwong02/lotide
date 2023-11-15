69d7c4939f472172d21b4f202b557308a4b08358
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-15 02:04:30 -0500

    created file

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+/* Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+
+The message will be similar to that of assertEqual. In fact, you could refer to the code for assertEqual to help you implement this. Instead of doing a simple === check though, this assertion function will make use of your eqArrays function for array comparison. */
+
+const eqArrays = function(x, y) {
+  if (x.length !== y.length) {
+    return false;
+  }
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] !== y[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  const isEqual = eqArrays(actual, expected);
+
+  if (isEqual) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
