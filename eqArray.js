0b6988ce5fe427b0b093f79905137967e45d9b4a
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-15 01:37:05 -0500

    removed some console.log notes

--- eqArray.js
+++ eqArray.js
@@ -27,8 +27,3 @@ eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
