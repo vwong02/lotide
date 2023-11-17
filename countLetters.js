da8a64ea60dc33597a4970f978c7e2649ef49058
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-16 21:15:51 -0500

    updated comments

--- countLetters.js
+++ countLetters.js
@@ -8,18 +8,22 @@ const assertEqual = function(actual, expected) {
 
 
 const countLetters = function(sentence) {
-  const obj = {};
-  for(const char of sentence){
-    if (char !== ' ')
-      if(obj[char]){
-        obj[char] += 1;
-      } else {
-        obj[char] = 1;
+
+  const obj = {};   //Create an empty object to store the counts of each letter
+  for(const char of sentence){ // Iterate through each character in the sentence
+    if (char !== ' ')// Check if the character is not a space
+      if(obj[char]){ // Check if the character is already a property in the object
+        obj[char] += 1;  // If yes, increment its count
+      } else { 
+        obj[char] = 1; // If no, create a new property with count 1
       }
   } 
-  return obj;
+  return obj; // Return the object containing the counts for each letter
 }
 
 const result = countLetters('hello, my name is Victoria');
-assertEqual(result['h'], 1);
-assertEqual(result['e'], 2);
+console.log(result['h']);
+console.log(result['e']);
+// assertEqual(result['h'], 1);
+// assertEqual(result['e'], 2);
+
