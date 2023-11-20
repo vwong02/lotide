15fc89b65b17d6239976e5bc538c407ec3426c4b
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-20 15:29:08 -0500

    added test file for tail.js

--- tail.js
+++ tail.js
@@ -1,31 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   return array.slice(1);
 };
 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-
-const j = [];
-tail(j);
-assertEqual(j.length, 3);
-
-const i = [4];
-tail(i);
-assertEqual(i.length, 3);
+module.exports = tail;
