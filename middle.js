96b60a767977e8580c6cb2347acb627744b6b200
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-20 16:08:03 -0500

    updated file

--- middle.js
+++ middle.js
@@ -1,52 +1,19 @@
-const eqArrays = function(x, y) {
-  if (x.length !== y.length) {
-    return false;
-  }
-  for (let i = 0; i < x.length; i++) {
-    if (x[i] !== y[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  const isEqual = eqArrays(actual, expected);
-
-  if (isEqual) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const middle = function(array) {
-// Arrays with 0-2 elements, return []
-    if (array.length <= 2) {
-      return [];
-// Even Numbers return two elements int he middle
-    } else if (array.length % 2 === 0) {
-      return array.slice(array.length / 2 - 1, array.length / 2 + 1);
-    } else {
-// Odd Numbers return single middle element
-      let middleArray = []
-      let middleIndex = Math.floor(array.length / 2);
-      middleArray.push(array[middleIndex])
-      return middleArray
-    }
+  // Arrays with 0-2 elements, return []
+  if (array.length <= 2) {
+    return [];
+    // Even Numbers return two elements int he middle
+  } else if (array.length % 2 === 0) {
+    return array.slice(array.length / 2 - 1, array.length / 2 + 1);
+  } else {
+    // Odd Numbers return single middle element
+    let middleArray = [];
+    let middleIndex = Math.floor(array.length / 2);
+    middleArray.push(array[middleIndex]);
+    return middleArray;
+  }
 };
 
-//TEST CASES
-// console.log(middle([1])); // => []
-// console.log(middle([1, 2])); // => []
-// console.log(middle([1, 2, 3])); // => [2]
-// console.log(middle([1, 2, 3, 4, 5])); // => [3]
-// console.log(middle([1, 2, 3, 4])); // => [2, 3]
-// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2 , 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+module.exports = middle;
