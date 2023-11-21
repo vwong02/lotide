6e22668e874e71f97d0298f4607f6cfd25fe0f68
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-20 19:33:03 -0500

    cleaned up test codes

--- test/headTest.js
+++ test/headTest.js
@@ -11,11 +11,11 @@ describe("#head", () => {
   });
 
   it("returns 5 for [5, 6, 7]", () => {
-    assert.strictEqual(head(['5']), '5');
+    assert.strictEqual(head(['5', '6', '7']), '5');
   });
 
   it("returns hello for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual(head(['Hello']), 'Hello');
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
   });
 
   it("returns [] for []", () => {
