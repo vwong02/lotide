2ea99db9f69074d0a5301972a87ff471fe651341
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-20 19:30:45 -0500

    updated files

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,31 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head([8]), 5);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns hello for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello']), 'Hello');
+  });
+
+  it("returns [] for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+});
+
+
+
+
+
+
